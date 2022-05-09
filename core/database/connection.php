ca5f2e4a63a13fa00bba8501e7f8913c363c40ce
YassineBouriga <64386005+YassineBouriga@users.noreply.github.com>
2022-05-09 07:56:21 +0100

    Add files via upload

---
+++ core/database/connection.php
@@ -0,0 +1,17 @@
+<?php
+// new connection file in classes folder
+
+// $servername = "localhost";
+// $db_name="twitter";
+// $username = "root";
+// $password = "";
+
+// try {
+//   $pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+//   // set the PDO error mode to exception
+//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// //   echo "Connected successfully";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
+?>
